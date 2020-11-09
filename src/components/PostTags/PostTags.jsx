6e38a3232aea14ae7525c391e76f3349266f6fb6
Vincenzo Marcovecchio <vinceoldmark@gmail.com>
2020-11-09 17:09:20 +0100

    fixes tags padding and links hopefully

--- src/components/PostTags/PostTags.jsx
+++ src/components/PostTags/PostTags.jsx
@@ -6,17 +6,17 @@ const StyledPostTags = styled.div`
   width: 95%;
   margin: 2rem auto;
   display: flex;
-  justify-content: flex-end;
-  align-items: flex-start;
+  justify-content: center;
+  align-items: center;
 
   a {
     background-color: white;
     text-decoration: none;
     color: black;
-    padding: 0 0.3rem;
+    padding: 0.3rem;
     border-radius: 0.3rem;
     border: 1px solid black;
-    margin-left:0.5rem;
+    margin-left: 0.5rem;
   }
 `;
 class PostTags extends Component {
