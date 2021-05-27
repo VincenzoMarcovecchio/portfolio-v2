4339f9fdc45bc0fb6e2e7b7cc6be5ecc87ece2ba
Vincenzo Marcovecchio <vinceoldmark@gmail.com>
2021-05-27 15:50:31 +0100

    change

--- src/components/search/search-box.js
+++ src/components/search/search-box.js
@@ -1,28 +1,6 @@
-
-import { connectSearchBox } from "react-instantsearch-dom"
-import { Search as SearchIcon } from "@styled-icons/fa-solid"
-
-export default connectSearchBox(
-  ({ refine, currentRefinement, className, onFocus }) => (
-    <form className={className}>
-      <input
-        className="SearchInput"
-        type="text"
-        placeholder="Search"
-        aria-label="Search"
-        onChange={e => refine(e.target.value)}
-        value={currentRefinement}
-        onFocus={onFocus}
-      />
-      <SearchIcon className="SearchIcon" />
-    </form>
-  )
-)
-
-
-
-import React from 'react';
-import { connectSearchBox } from 'react-instantsearch-dom';
+import React from "react";
+import { connectSearchBox } from "react-instantsearch-dom";
+import { Search as SearchIcon } from "@styled-icons/fa-solid";
 
 export default connectSearchBox(
   ({ refine, currentRefinement, className, onFocus }) => (
@@ -36,6 +14,7 @@ export default connectSearchBox(
         value={currentRefinement}
         onFocus={onFocus}
       />
+      <SearchIcon className="SearchIcon" />
     </form>
   )
-)
+);
