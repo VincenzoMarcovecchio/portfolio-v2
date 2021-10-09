41115d7eddd32736ae2d4c1d9f680fd4ab8b8ae4
unknown <vinceoldmark@gmail.com>
2021-10-09 21:53:23 +0100

    major changes

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,9 +1,9 @@
-const urljoin = require('url-join')
-const path = require('path')
-const config = require('./data/SiteConfig')
+const urljoin = require("url-join");
+const path = require("path");
+const config = require("./data/SiteConfig");
 
 module.exports = {
-  pathPrefix: config.pathPrefix === '' ? '/' : config.pathPrefix,
+  pathPrefix: config.pathPrefix === "" ? "/" : config.pathPrefix,
   siteMetadata: {
     siteUrl: urljoin(config.siteUrl, config.pathPrefix),
     rssMetadata: {
@@ -13,46 +13,48 @@ module.exports = {
       description: config.siteDescription,
       image_url: `${urljoin(
         config.siteUrl,
-        config.pathPrefix,
+        config.pathPrefix
       )}/logos/logo-512.png`,
       copyright: config.copyright,
     },
   },
-  
+
   plugins: [
-    'gatsby-plugin-styled-components',
-    'gatsby-plugin-react-helmet',
-    'gatsby-plugin-lodash',
-    'gatsby-plugin-sitemap',
+    "gatsby-plugin-styled-components",
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-lodash",
+
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: 'assets',
+        name: "assets",
         path: `${__dirname}/static/`,
       },
     },
 
     {
-      resolve: 'gatsby-source-custom-api',
+      resolve: "gatsby-source-custom-api",
       options: {
         url: {
-          development: 'https://api.cfptime.org/api/upcoming/', // on "gatsby develop"
-          production: 'https://api.cfptime.org/api/upcoming/', // on "gatsby build"
+          development: "https://api.cfptime.org/api/upcoming/", // on "gatsby develop"
+          production: "https://api.cfptime.org/api/upcoming/", // on "gatsby build"
         },
-        rootKey: 'eventi',
+        rootKey: "eventi",
       },
     },
     {
-      resolve: 'gatsby-source-custom-api',
+      resolve: "gatsby-source-custom-api",
       options: {
         url: {
-          development: 'https://raw.githubusercontent.com/projectdiscovery/public-bugbounty-programs/master/chaos-bugbounty-list.json', // on "gatsby develop"
-          production: 'https://raw.githubusercontent.com/projectdiscovery/public-bugbounty-programs/master/chaos-bugbounty-list.json', // on "gatsby build"
+          development:
+            "https://raw.githubusercontent.com/projectdiscovery/public-bugbounty-programs/master/chaos-bugbounty-list.json", // on "gatsby develop"
+          production:
+            "https://raw.githubusercontent.com/projectdiscovery/public-bugbounty-programs/master/chaos-bugbounty-list.json", // on "gatsby build"
         },
-        rootKey: 'bugs',
+        rootKey: "bugs",
       },
     },
- 
+
     {
       resolve: `gatsby-plugin-layout`,
       options: {
@@ -60,73 +62,72 @@ module.exports = {
       },
     },
 
-    
     {
-      resolve: 'gatsby-source-filesystem',
+      resolve: "gatsby-source-filesystem",
       options: {
-        name: 'posts',
+        name: "posts",
         path: `${__dirname}/content/`,
       },
     },
     {
-      resolve: 'gatsby-transformer-remark',
+      resolve: "gatsby-transformer-remark",
       options: {
         plugins: [
           {
             resolve: `gatsby-remark-relative-images`,
           },
           {
-            resolve: 'gatsby-remark-images',
+            resolve: "gatsby-remark-images",
             options: {
               maxWidth: 690,
             },
           },
           {
-            resolve: 'gatsby-remark-responsive-iframe',
+            resolve: "gatsby-remark-responsive-iframe",
           },
-          'gatsby-remark-copy-linked-files',
-          'gatsby-remark-autolink-headers',
-          'gatsby-remark-prismjs',
+          "gatsby-remark-copy-linked-files",
+          "gatsby-remark-autolink-headers",
+          "gatsby-remark-prismjs",
         ],
       },
     },
     {
-      resolve: 'gatsby-plugin-google-analytics',
+      resolve: "gatsby-plugin-google-analytics",
       options: {
         trackingId: config.googleAnalyticsID,
       },
     },
 
     {
-      resolve: 'gatsby-plugin-nprogress',
+      resolve: "gatsby-plugin-nprogress",
       options: {
         color: config.themeColor,
       },
     },
-    'gatsby-plugin-sharp',
-    'gatsby-transformer-sharp',
-    'gatsby-plugin-catch-links',
-    'gatsby-plugin-twitter',
-   
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp",
+    "gatsby-plugin-catch-links",
+    "gatsby-plugin-twitter",
+
     {
-      resolve: 'gatsby-plugin-robots-txt',
+      resolve: "gatsby-plugin-robots-txt",
       options: {
-        host: 'https://www.vincenzo.codes',
-        sitemap: 'https://www.vincenzo.codes/sitemap.xml',
-        policy: [{ userAgent: '*', allow: '/' }],
+        host: "https://www.vincenzo.codes",
+        sitemap: "https://www.vincenzo.codes/sitemap.xml",
+        policy: [{ userAgent: "*", allow: "/" }],
       },
     },
     {
-      resolve: 'gatsby-plugin-manifest',
+      resolve: "gatsby-plugin-manifest",
       options: {
         name: "Vincenzo's portfolio",
-        short_name: 'Vincenzo Dev',
+        short_name: "Vincenzo Dev",
         description: config.siteDescription,
         start_url: `/`,
         background_color: config.backgroundColor,
-        theme_color: '#57b3ab',
-        display: 'minimal-ui',
-        icon: 'src/favicon/engine.png',
+        theme_color: "#57b3ab",
+        display: "minimal-ui",
+        icon: "src/favicon/engine.png",
         lang: `en`,
         icon_options: {
           purpose: `any maskable`,
@@ -135,24 +136,24 @@ module.exports = {
     },
 
     {
-      resolve: 'gatsby-plugin-netlify-cms',
+      resolve: "gatsby-plugin-netlify-cms",
       options: {
-        modulePath: path.resolve('src/netlifycms/index.js'), // default: undefined
+        modulePath: path.resolve("src/netlifycms/index.js"), // default: undefined
         enableIdentityWidget: true,
-        publicPath: 'admin',
-        htmlTitle: 'Content Manager',
+        publicPath: "admin",
+        htmlTitle: "Content Manager",
         includeRobots: false,
       },
     },
 
     {
-      resolve: 'gatsby-plugin-feed',
+      resolve: "gatsby-plugin-feed",
       options: {
         setup(ref) {
-          const ret = ref.query.site.siteMetadata.rssMetadata
-          ret.allMarkdownRemark = ref.query.allMarkdownRemark
-          ret.generator = 'vincenzo'
-          return ret
+          const ret = ref.query.site.siteMetadata.rssMetadata;
+          ret.allMarkdownRemark = ref.query.allMarkdownRemark;
+          ret.generator = "vincenzo";
+          return ret;
         },
         query: `
         {
@@ -173,7 +174,7 @@ module.exports = {
         feeds: [
           {
             serialize(ctx) {
-              const { rssMetadata } = ctx.query.site.siteMetadata
+              const { rssMetadata } = ctx.query.site.siteMetadata;
               return ctx.query.allMarkdownRemark.edges.map((edge) => ({
                 categories: edge.node.frontmatter.tags,
                 date: edge.node.fields.date,
@@ -182,10 +183,10 @@ module.exports = {
                 url: rssMetadata.site_url + edge.node.fields.slug,
                 guid: rssMetadata.site_url + edge.node.fields.slug,
                 custom_elements: [
-                  { 'content:encoded': edge.node.html },
+                  { "content:encoded": edge.node.html },
                   { author: config.userEmail },
                 ],
-              }))
+              }));
             },
             query: `
             {
@@ -226,11 +227,11 @@ module.exports = {
       options: {
         appId: `779LNWKXIK`,
         apiKey: `56936b4655e9cc8b1c2a7cffd9b2a43e`,
-        queries: require('./src/utils/algolia-queries'),
+        queries: require("./src/utils/algolia-queries"),
       },
     },
-
+    "gatsby-plugin-sitemap",
     `gatsby-plugin-preact`,
     `gatsby-plugin-offline`,
   ],
-}
+};
