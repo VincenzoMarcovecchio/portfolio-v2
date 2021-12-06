44576d11272298fac9cc8969aca086b3d94cc5e3
unknown <vinceoldmark@gmail.com>
2021-12-06 19:40:34 +0000

    change2

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,7 @@
+import fetch from "node-fetch";
 const path = require("path");
 const _ = require("lodash");
 const moment = require("moment");
-const fetch = require("node-fetch");
 const siteConfig = require("./data/SiteConfig");
 
 exports.onCreateNode = ({ node, actions, getNode }) => {
