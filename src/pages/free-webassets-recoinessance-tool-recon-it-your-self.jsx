859510802b67d6761daf689a10bdbf0e722a8081
Vincenzo Marcovecchio <55591572+VincenzoMarcovecchio@users.noreply.github.com>
2022-06-21 13:46:08 +0200

    change

---
+++ src/pages/free-webassets-recoinessance-tool-recon-it-your-self.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+
+const Test = () => (
+    <div>
+        {// ---
+            // title: "Server Side Rendering in Gatsby v4"
+            // cover: "sample-exam-ISTQB-foundational-level-2018-sillabus-a"
+            // date: "2022-06-22"
+            // category: "test"
+            // slug: "gatsby-server-side-rendering-the-nextjs-way"
+
+            // tags:
+            //   - gatsby
+            // ---
+
+
+            // I know you like Nextjs but you can't forget, you must not forget your first love Gatsby.js. With the release of the new version v4 I had to go back and look it up to see what has changed. Deferred static generation is a feature that I like a lot but in this post I'm going to show you how I rendered a function at server side using Gatsby.js
+        }
+        <h1>NOT FOUND</h1>
+        <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    </div>
+);
+
+export default React.memo(Test);
