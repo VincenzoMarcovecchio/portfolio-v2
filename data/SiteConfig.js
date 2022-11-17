8e58999b733d1c02aece5c790fd983cb8fd25d3b
Vincenzo Marcovecchio <55591572+VincenzoMarcovecchio@users.noreply.github.com>
2022-11-17 09:20:51 +0100

    hh

--- data/SiteConfig.js
+++ data/SiteConfig.js
@@ -2,7 +2,7 @@ const config = {
   siteTitle: `Vincenzo's codes`, // Site title.
   siteTitleShort: 'A Web Developer Portfolio', // Short site title for homescreen (PWA). Preferably should be under 12 characters to prevent truncation.
   siteTitleAlt: 'A Web Developer Portfolio', // Alternative site title for SEO.
-  siteLogo: './favicon/engine.png', // Logo used for SEO and manifest.
+  siteLogo: 'engine.png', // Logo used for SEO and manifest.
   siteUrl: 'https://vincenzo.codes', // Domain of your website without pathPrefix.
   pathPrefix: '/', // gatsby buildPrefixes all links. For cases when deployed to example.github.io/gatsby-advanced-starter/.
   siteDescription:
