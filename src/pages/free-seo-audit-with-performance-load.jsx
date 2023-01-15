be4b4facabeab5a0cc0658ecd39736f173197966
Vincenzo Marcovecchio <55591572+VincenzoMarcovecchio@users.noreply.github.com>
2023-01-15 08:38:57 +0100

    asd

--- src/pages/free-seo-audit-with-performance-load.jsx
+++ src/pages/free-seo-audit-with-performance-load.jsx
@@ -1,20 +1,173 @@
 import React from 'react';
 import { Helmet } from "react-helmet";
+import styled from "styled-components";
+import { device } from '../styles/Global';
 
+const StyledAbout = styled.section`
+  min-height: 100vh;
+  max-width: 90vw;
+  text-align: center;
+  max-width: 600px;
+  margin: auto;
+  padding: 3rem 0;
+
+  span {
+    line-height: 1.3rem;
+  }
+  .my__form {
+    width: 90%;
+    max-width: 600px;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-around;
+    margin: 3rem auto;
+  }
+
+  .form-group {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    justify-content: center;
+    width: 100%;
+  }
+
+  label {
+    margin-top: 1rem;
+  }
+
+  input {
+    box-sizing: border-box;
+    width: 100%;
+    height: 2.3rem;
+    padding-left: 0.6rem;
+    font-family: inherit;
+    background-color: rgb(255, 255, 255);
+    background-clip: padding-box;
+    border: 1px solid rgb(34, 37, 42);
+    border-radius: 0.25rem;
+    -webkit-transition: border-color 0.15s ease-in-out,
+      box-shadow 0.15s ease-in-out;
+    transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+  }
+  input:focus {
+    outline: none;
+    background-color: rgb(255, 255, 255);
+    border-color: rgb(181, 186, 245);
+    box-shadow: 0 5px 15px 0 rgba(0, 0, 0, 0.15);
+  }
+  textarea {
+    box-sizing: border-box;
+    width: 100%;
+    height: 7rem;
+    padding-left: 0.6rem;
+    font-family: inherit;
+    padding-top: 0.6rem;
+    background-color: rgb(255, 255, 255);
+    background-clip: padding-box;
+    border: 1px solid rgb(34, 37, 42);
+    border-radius: 0.25rem;
+    -webkit-transition: border-color 0.15s ease-in-out,
+      box-shadow 0.15s ease-in-out;
+    transition: border-color 0.15s ease-in-out, box-shadow 0.15s ease-in-out;
+  }
+  textarea:focus {
+    outline: none;
+    background-color: rgb(255, 255, 255);
+    border-color: rgb(181, 186, 245);
+    box-shadow: 0 5px 15px 0 rgba(0, 0, 0, 0.15);
+  }
+  button {
+    height: 2.3rem;
+    width: 8rem;
+    cursor: pointer;
+    margin-top: 2rem;
+    border: 1px solid rgb(217, 220, 225);
+    border-radius: 0.25rem;
+    color: white;
+    font-size: 18px;
+    background-color: #4f7dfd;
+  }
+
+  h1 {
+    margin-top: unset;
+    font-size: 2.3rem;
+    line-height: 1.2;
+    width: 95%;
+    margin: auto;
+    @media ${device.mobileL} {
+      font-size: 1.9rem;
+    }
+  }
+  h3 {
+    width: 95%;
+    margin: 2rem auto;
+  }
+
+  img {
+    margin-bottom: 4rem;
+    width: 100%;
+  }
+  span {
+    color: inherit;
+    margin-bottom: 4rem;
+  }
+  .cv-container {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    width: 80%;
+    margin: auto;
+  }
+
+  #Layer_1 {
+    animation: fill 2s linear infinite;
+  }
+
+  path {
+    stroke-dasharray: 0 1000;
+    animation: draw 2s linear infinite;
+    stroke: black;
+  }
+
+  @keyframes draw {
+    from {
+      stroke-dasharray: 0 1000;
+    }
+    to {
+      stroke-dasharray: 1000 0;
+    }
+  }
+
+  @keyframes fill {
+    from {
+      fill: none;
+    }
+    to {
+      fill: black;
+    }
+  }
+`;
 function seo() {
 
 
 
   return (
     <>
-      <Helmet>
-        <script src="/seo.js" type="text/javascript" />
-      </Helmet>
+      <StyledAbout>
+        <Helmet>
+          <script src="/seo.js" type="text/javascript" />
+        </Helmet>
 
-      <div id="body">
-        <input type="text" />
-      </div>
+        <div id="body">
+          <form>
+            <label for="inputId">Url</label>
+            <input id="inputId" placeholder="es https://developers.google" type="text" />
+            <button type="submit">Run Audit</button>
+          </form>
+        </div>
+      </StyledAbout>
     </>
+
   );
 
 };
--- static/seo.js
+++ static/seo.js
@@ -1,34 +1,41 @@
 const url = setUpQuery();
-fetch(url)
-  .then(response => response.json())
-  .then(json => {
-    // See https://developers.google.com/speed/docs/insights/v5/reference/pagespeedapi/runpagespeed#response
-    // to learn more about each of the properties in the response object.
-    showInitialContent(json.id);
-    const cruxMetrics = {
-      "First Contentful Paint": json.loadingExperience.metrics.FIRST_CONTENTFUL_PAINT_MS.category,
-      "First Input Delay": json.loadingExperience.metrics.FIRST_INPUT_DELAY_MS.category
-    };
-    showCruxContent(cruxMetrics);
-    const lighthouse = json.lighthouseResult;
-    console.log(json);
-    console.log(lighthouse);
-    const lighthouseMetrics = {
-      'First Contentful Paint': lighthouse.audits['first-contentful-paint'].displayValue,
-      'Speed Index': lighthouse.audits['speed-index'].displayValue,
-      'Time To Interactive': lighthouse.audits['interactive'].displayValue,
-      'First Meaningful Paint': lighthouse.audits['first-meaningful-paint'].displayValue,
-      'First CPU Idle': lighthouse.audits['first-cpu-idle'].displayValue,
-      'Estimated Input Latency': lighthouse.audits['estimated-input-latency'].displayValue
-    };
-    showLighthouseContent(lighthouseMetrics);
-  });
+
+var form = document.getElementsByTagName("form");
+form.addEventListener("submit", function(evt) {
+      evt.preventDefault();
+      fetch(url)
+      .then(response => response.json())
+      .then(json => {
+        // See https://developers.google.com/speed/docs/insights/v5/reference/pagespeedapi/runpagespeed#response
+        // to learn more about each of the properties in the response object.
+        showInitialContent(json.id);
+        const cruxMetrics = {
+          "First Contentful Paint": json.loadingExperience.metrics.FIRST_CONTENTFUL_PAINT_MS.category,
+          "First Input Delay": json.loadingExperience.metrics.FIRST_INPUT_DELAY_MS.category
+        };
+        showCruxContent(cruxMetrics);
+        const lighthouse = json.lighthouseResult;
+        console.log(json);
+        console.log(lighthouse);
+        const lighthouseMetrics = {
+          'First Contentful Paint': lighthouse.audits['first-contentful-paint'].displayValue,
+          'Speed Index': lighthouse.audits['speed-index'].displayValue,
+          'Time To Interactive': lighthouse.audits['interactive'].displayValue,
+          'First Meaningful Paint': lighthouse.audits['first-meaningful-paint'].displayValue,
+        };
+        showLighthouseContent(lighthouseMetrics);
+      });
+        
+    }
+  
+});
 
 
 function setUpQuery() {
 const api = 'https://www.googleapis.com/pagespeedonline/v5/runPagespeed';
+const queryUrl = document.getElementById("inputId").value;
 const parameters = {
-  url: encodeURIComponent('https://developers.google.com')
+  url: encodeURIComponent(queryUrl)
 };
 let query = `${api}?`;
         for (key in parameters) {
