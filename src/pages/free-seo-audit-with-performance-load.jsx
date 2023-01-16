505a2185f3b1d0973436144ff5d0efe09327fc37
Vincenzo Marcovecchio <55591572+VincenzoMarcovecchio@users.noreply.github.com>
2023-01-16 08:55:22 +0100

    as

--- src/pages/free-seo-audit-with-performance-load.jsx
+++ src/pages/free-seo-audit-with-performance-load.jsx
@@ -149,56 +149,7 @@ const StyledAbout = styled.section`
 `;
 function seo() {
 
-React.useEffect(()=> {
 
-const url = setUpQuery();
-
-  function setUpQuery() {
-    const api = 'https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=';
-    let queryUrl = document.querySelectorAll("#inputId").value
-    const parameters = {
-      url: encodeURIComponent(queryUrl)
-    };
-    let query = `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${queryUrl}?`;
-    return query
-  }
-
-
-  function showInitialContent(id) {
-
-    const body = document.getElementById("body")
-    const title = document.createElement('h1');
-    title.textContent = 'PageSpeed Insights API Demo';
-    body.appendChild(title);
-    const page = document.createElement('p');
-    page.textContent = `Page tested: ${id}`;
-    body.appendChild(page);
-  }
-
-  function showCruxContent(cruxMetrics) {
-    const body = document.getElementById("body")
-    const cruxHeader = document.createElement('h2');
-    cruxHeader.textContent = "Chrome User Experience Report Results";
-    body.appendChild(cruxHeader);
-    for (key in cruxMetrics) {
-      const p = document.createElement('p');
-      p.textContent = `${key}: ${cruxMetrics[key]}`;
-      body.appendChild(p);
-    }
-  }
-
-  function showLighthouseContent(lighthouseMetrics) {
-    const body = document.getElementById("body")
-    const lighthouseHeader = document.createElement('h2');
-    lighthouseHeader.textContent = "Lighthouse Results";
-    body.appendChild(lighthouseHeader);
-    for (key in lighthouseMetrics) {
-      const p = document.createElement('p');
-      p.textContent = `${key}: ${lighthouseMetrics[key]}`;
-      body.appendChild(p);
-    }
-  }
-},[])
   
 
 
@@ -227,6 +178,7 @@ const url = setUpQuery();
   will look on Google, Facebook, Twitter and more!"/>
         <meta property="twitter:image"
           content="https://metatags.io/assets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+      <script type="application/javascript" src="/seo.js"></script>
       </Helmet>
       <StyledAbout>
         <div id="body">
--- static/seo.js
+++ static/seo.js
@@ -1,82 +1,47 @@
 const url = setUpQuery();
 
-var form = document.querySelectorAll("#formId");
-form.addEventListener("submit", function(evt) {
-      evt.preventDefault();
-      evt.stopPropagation();
-      fetch(url)
-      .then(response => response.json())
-      .then(json => {
-        // See https://developers.google.com/speed/docs/insights/v5/reference/pagespeedapi/runpagespeed#response
-        // to learn more about each of the properties in the response object.
-        showInitialContent(json.id);
-        const cruxMetrics = {
-          "First Contentful Paint": json.loadingExperience.metrics.FIRST_CONTENTFUL_PAINT_MS.category,
-          "First Input Delay": json.loadingExperience.metrics.FIRST_INPUT_DELAY_MS.category
-        };
-        showCruxContent(cruxMetrics);
-        const lighthouse = json.lighthouseResult;
-        console.log(json);
-        console.log(lighthouse);
-        const lighthouseMetrics = {
-          'First Contentful Paint': lighthouse.audits['first-contentful-paint'].displayValue,
-          'Speed Index': lighthouse.audits['speed-index'].displayValue,
-          'Time To Interactive': lighthouse.audits['interactive'].displayValue,
-          'First Meaningful Paint': lighthouse.audits['first-meaningful-paint'].displayValue,
-        };
-        showLighthouseContent(lighthouseMetrics);
-      });
-        
-  
-});
+  function setUpQuery() {
+    const api = 'https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=';
+    let queryUrl = document.querySelectorAll("#inputId").value
+    const parameters = {
+      url: encodeURIComponent(queryUrl)
+    };
+    let query = `https://www.googleapis.com/pagespeedonline/v5/runPagespeed?url=${queryUrl}?`;
+    return query
+  }
 
 
-function setUpQuery() {
-const api = 'https://www.googleapis.com/pagespeedonline/v5/runPagespeed';
-let queryUrl = document.querySelectorAll("#inputId").value
-const parameters = {
-  url: encodeURIComponent(queryUrl)
-};
-let query = `${api}?`;
-        for (key in parameters) {
-          query += `${key}=${parameters[key]}`;
-}
-        return query;
-}
+  function showInitialContent(id) {
 
-        function showInitialContent(id) {
-          
-        const body =document.getElementById("body")
-        const title = document.createElement('h1');
-        title.textContent = 'PageSpeed Insights API Demo';
-        body.appendChild(title);
-        const page = document.createElement('p');
-        page.textContent = `Page tested: ${id}`;
-        body.appendChild(page);
-}
+    const body = document.getElementById("body")
+    const title = document.createElement('h1');
+    title.textContent = 'PageSpeed Insights API Demo';
+    body.appendChild(title);
+    const page = document.createElement('p');
+    page.textContent = `Page tested: ${id}`;
+    body.appendChild(page);
+  }
 
-        function showCruxContent(cruxMetrics) {
-                const body =document.getElementById("body")
-const cruxHeader = document.createElement('h2');
-        cruxHeader.textContent = "Chrome User Experience Report Results";
-        body.appendChild(cruxHeader);
-        for (key in cruxMetrics) {
-  const p = document.createElement('p');
-        p.textContent = `${key}: ${cruxMetrics[key]}`;
-        body.appendChild(p);
-}
-}
+  function showCruxContent(cruxMetrics) {
+    const body = document.getElementById("body")
+    const cruxHeader = document.createElement('h2');
+    cruxHeader.textContent = "Chrome User Experience Report Results";
+    body.appendChild(cruxHeader);
+    for (key in cruxMetrics) {
+      const p = document.createElement('p');
+      p.textContent = `${key}: ${cruxMetrics[key]}`;
+      body.appendChild(p);
+    }
+  }
 
-        function showLighthouseContent(lighthouseMetrics) {
-                const body =document.getElementById("body")
-                const lighthouseHeader = document.createElement('h2');
-        lighthouseHeader.textContent = "Lighthouse Results";
-        body.appendChild(lighthouseHeader);
-        for (key in lighthouseMetrics) {
-  const p = document.createElement('p');
-        p.textContent = `${key}: ${lighthouseMetrics[key]}`;
-        body.appendChild(p);
-}
-}
-
-
+  function showLighthouseContent(lighthouseMetrics) {
+    const body = document.getElementById("body")
+    const lighthouseHeader = document.createElement('h2');
+    lighthouseHeader.textContent = "Lighthouse Results";
+    body.appendChild(lighthouseHeader);
+    for (key in lighthouseMetrics) {
+      const p = document.createElement('p');
+      p.textContent = `${key}: ${lighthouseMetrics[key]}`;
+      body.appendChild(p);
+    }
+  }
