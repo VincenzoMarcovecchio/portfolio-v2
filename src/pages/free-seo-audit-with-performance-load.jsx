9f1884b1fc80db7a2089d8ed06b27a9b48aa0cec
Vincenzo Marcovecchio <55591572+VincenzoMarcovecchio@users.noreply.github.com>
2023-01-16 20:21:42 +0100

    asd

--- src/pages/free-seo-audit-with-performance-load.jsx
+++ src/pages/free-seo-audit-with-performance-load.jsx
@@ -165,39 +165,150 @@ const [result, setResult] = React.useState({})
   }
 
 
-  function showInitialContent(id) {
+  var pubs = result
 
-    const body = typeof window !== "undefined" && document.getElementById("body")
-    const title = typeof window !== "undefined" && document.createElement('h1');
-    title.textContent = 'PageSpeed Insights API Demo';
-    body.appendChild(title);
-    const page = typeof window !== "undefined" && document.createElement('p');
-    page.textContent = `Page tested: ${id}`;
-    body.appendChild(page);
+  
+  var diameter = 800;
+  
+  var margin = {top: 20, right: 120, bottom: 20, left: 120},
+      width = diameter,
+      height = diameter;
+      
+  var i = 0,
+      duration = 350,
+      root;
+  
+  var tree = d3.layout.tree()
+      .size([360, diameter / 2 - 80])
+      .separation(function(a, b) { return (a.parent == b.parent ? 1 : 10) / a.depth; });
+  
+  var diagonal = d3.svg.diagonal.radial()
+      .projection(function(d) { return [d.y, d.x / 180 * Math.PI]; });
+  
+  var svg = d3.select("body").append("svg")
+      .attr("width", width )
+      .attr("height", height )
+    .append("g")
+      .attr("transform", "translate(" + diameter / 2 + "," + diameter / 2 + ")");
+  
+  root = pubs;
+  root.x0 = height / 2;
+  root.y0 = 0;
+  
+  //root.children.forEach(collapse); // start with all children collapsed
+  update(root);
+  
+  d3.select(self.frameElement).style("height", "800px");
+  
+  function update(source) {
+  
+    // Compute the new tree layout.
+    var nodes = tree.nodes(root),
+        links = tree.links(nodes);
+  
+    // Normalize for fixed-depth.
+    nodes.forEach(function(d) { d.y = d.depth * 80; });
+  
+    // Update the nodes…
+    var node = svg.selectAll("g.node")
+        .data(nodes, function(d) { return d.id || (d.id = ++i); });
+  
+    // Enter any new nodes at the parent's previous position.
+    var nodeEnter = node.enter().append("g")
+        .attr("class", "node")
+        //.attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")"; })
+        .on("click", click);
+  
+    nodeEnter.append("circle")
+        .attr("r", 1e-6)
+        .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
+  
+    nodeEnter.append("text")
+        .attr("x", 10)
+        .attr("dy", ".35em")
+        .attr("text-anchor", "start")
+        //.attr("transform", function(d) { return d.x < 180 ? "translate(0)" : "rotate(180)translate(-" + (d.name.length * 8.5)  + ")"; })
+        .text(function(d) { return d.name; })
+        .style("fill-opacity", 1e-6);
+  
+    // Transition nodes to their new position.
+    var nodeUpdate = node.transition()
+        .duration(duration)
+        .attr("transform", function(d) { return "rotate(" + (d.x - 90) + ")translate(" + d.y + ")"; })
+  
+    nodeUpdate.select("circle")
+        .attr("r", 4.5)
+        .style("fill", function(d) { return d._children ? "lightsteelblue" : "#fff"; });
+  
+    nodeUpdate.select("text")
+        .style("fill-opacity", 1)
+        .attr("transform", function(d) { return d.x < 180 ? "translate(0)" : "rotate(180)translate(-" + (d.name.length + 50)  + ")"; });
+  
+    // TODO: appropriate transform
+    var nodeExit = node.exit().transition()
+        .duration(duration)
+        //.attr("transform", function(d) { return "diagonal(" + source.y + "," + source.x + ")"; })
+        .remove();
+  
+    nodeExit.select("circle")
+        .attr("r", 1e-6);
+  
+    nodeExit.select("text")
+        .style("fill-opacity", 1e-6);
+  
+    // Update the links…
+    var link = svg.selectAll("path.link")
+        .data(links, function(d) { return d.target.id; });
+  
+    // Enter any new links at the parent's previous position.
+    link.enter().insert("path", "g")
+        .attr("class", "link")
+        .attr("d", function(d) {
+          var o = {x: source.x0, y: source.y0};
+          return diagonal({source: o, target: o});
+        });
+  
+    // Transition links to their new position.
+    link.transition()
+        .duration(duration)
+        .attr("d", diagonal);
+  
+    // Transition exiting nodes to the parent's new position.
+    link.exit().transition()
+        .duration(duration)
+        .attr("d", function(d) {
+          var o = {x: source.x, y: source.y};
+          return diagonal({source: o, target: o});
+        })
+        .remove();
+  
+    // Stash the old positions for transition.
+    nodes.forEach(function(d) {
+      d.x0 = d.x;
+      d.y0 = d.y;
+    });
   }
-
-  function showCruxContent(cruxMetrics) {
-    const body = typeof window !== "undefined" && document.getElementById("body")
-    const cruxHeader = typeof window !== "undefined" && document.createElement('h2');
-    cruxHeader.textContent = "Chrome User Experience Report Results";
-    body.appendChild(cruxHeader);
-    for (key in cruxMetrics) {
-      const p =  typeof window !== "undefined" && document.createElement('p');
-      p.textContent = `${key}: ${cruxMetrics[key]}`;
-      body.appendChild(p);
+  
+  // Toggle children on click.
+  function click(d) {
+    if (d.children) {
+      d._children = d.children;
+      d.children = null;
+    } else {
+      d.children = d._children;
+      d._children = null;
     }
+    
+    update(d);
   }
-
-  function showLighthouseContent(lighthouseMetrics) {
-    const body = typeof window !== "undefined" && document.getElementById("body")
-    const lighthouseHeader = typeof window !== "undefined" && document.createElement('h2');
-    lighthouseHeader.textContent = "Lighthouse Results";
-    body.appendChild(lighthouseHeader);
-    for (key in lighthouseMetrics) {
-      const p = typeof window !== "undefined" && document.createElement('p');
-      p.textContent = `${key}: ${lighthouseMetrics[key]}`;
-      body.appendChild(p);
-    }
+  
+  // Collapse nodes
+  function collapse(d) {
+    if (d.children) {
+        d._children = d.children;
+        d._children.forEach(collapse);
+        d.children = null;
+      }
   }
   
 
@@ -222,6 +333,7 @@ const [result, setResult] = React.useState({})
         <meta property="twitter:description" content="A simple tool to generate google lighthouse reports with more in depth details!"/>
         <meta property="twitter:image"
           content="https://vincenzo.codes/free-seo-audits-with-performance-loadassets/meta-tags-16a33a6a8531e519cc0936fbba0ad904e52d35f34a46c97a2c9f6f7dd7d336f2.png" />
+    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/d3/3.3.10/d3.min.js"></script>
       </Helmet>
       <StyledAbout>
         <div id="body">
@@ -241,7 +353,18 @@ const [result, setResult] = React.useState({})
             <button type="submit">Run Audit</button>
           </form>
         
-<div>Results for: {result.captchaResult}</div>
+<div>Results for: {result.id}</div>
+<small>{result.lighthouseResult.lighthouseVersion}</small>
+<small>{result.lighthouseResult.userAgent}</small>
+       <h3>Audits:</h3>
+       <div className="cols">
+<div className="card">
+  <h4>{result.lighthouseResult.audits.bootup-time.title}</h4>
+  <span>{result.lighthouseResult.audits.bootup-time.numericValue} {result.lighthouseResult.audits.bootup-time.numericUnit}</span>
+  <span>{result.lighthouseResult.audits.bootup-time.description} </span>
+</div>
+
+       </div>
         </div>
       </StyledAbout>
     </>
